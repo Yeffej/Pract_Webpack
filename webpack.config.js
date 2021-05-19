16830dfb0b06fb17b464c25e9a4251a9e76bc81c
Yefri Encarnación <yefriej128@gmail.com>
2021-05-19 11:13:49 -0400

    Se añaden loader para html, css y imagenes; se integra babel con webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
-const path = require("path")
+const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+const miniCssExtract = require("mini-css-extract-plugin")
+const copyPlugin = require("copy-webpack-plugin")
 
 /** @type {import('webpack').Configuration} */
 
@@ -6,7 +9,8 @@ module.exports = {
     entry: "./src/index.js",
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "Main.js"
+        filename: "Main.js",
+        assetModuleFilename: "Images/[hash][ext]"
     },
     resolve: {
         extensions: [".js"],
@@ -20,7 +24,31 @@ module.exports = {
                     loader: "babel-loader"
                 }
                 
+            },
+            {
+                test: /\.css$/i,
+                use: [miniCssExtract.loader, "css-loader"]
+            },
+            {
+                test: /\.png$/i,
+                type: "asset/resource"
             }
         ]
-    }
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            inject: true,
+            template: "./public/index.html",
+            filename: "index.html"
+        }),
+        new miniCssExtract(),
+        new copyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, "src", "assets/images"),
+                    to: "Assets/images"
+                }
+            ]
+        })
+    ]
 }
